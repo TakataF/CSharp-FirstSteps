9a2d12c623dddbf18f4fb1e8523681bc62a6a32e
Felipe <Felipe@FELIPE>
2024-02-20 23:41:44 -0300

    Adicionar arquivos de projeto.

---
+++ Ler Numeros/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite um Numero: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("O Numero: " + numero + " é Par.");
+}
+else
+{
+    Console.WriteLine("O Numero: " + numero + " é Ímpar.");
+}
---
+++ Numeros/Program.cs
@@ -0,0 +1,7 @@
+﻿int idade = 20;
+double altura = 1.75;
+string nome = "Felipe";
+
+Console.WriteLine("Idade: " + idade);
+Console.WriteLine("Altura: " + altura);
+Console.WriteLine("Nome: " + nome);
