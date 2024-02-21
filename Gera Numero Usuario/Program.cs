cd69918bfe10b1db9f4f1c1004fd8bf60f761abc
Felipe Takata de Alvarenga <felipetakatadealvarenga@gmail.com>
2024-02-21 10:26:05 -0300

    Exercicios de Numeros Aleatorios

---
+++ Gera Numero Usuario/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Quantos Numeros Aleatorios Deseja: ");
+int limiteAleatorio = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Qual o Numero Limite Maximo: ");
+int numeroLimite = Convert.ToInt32(Console.ReadLine());
+
+Random randnum = new Random();
+
+for (int i = 0; i < limiteAleatorio; i++) 
+{ 
+Console.WriteLine(randnum.Next(0, numeroLimite));
+}
---
+++ Gerar Numero/Program.cs
@@ -0,0 +1,4 @@
+﻿Random randNum = new Random();
+for (int i = 0; i < 20; i++)
+    Console.WriteLine(randNum.Next(0, 1000));
+
